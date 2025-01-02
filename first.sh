8e34f37be808d71912a0e3291a7f2abf390769e4
HARMANJOT SINGH NANDA <154700618+harmannanda1234@users.noreply.github.com>
2025-01-02 23:03:07 +0530

    first shell script for system monitoring basic
    here is the script which monitors the cpu and ram using bash shel scripting

---
+++ first.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "welcome to system monitoring script "
+
+#cpu usage:
+echo " the cpu info is listed bellow:"
+lscpu
+
+#the ram usage 
+echo "the ram usage of system: "
+free -h
+
+
